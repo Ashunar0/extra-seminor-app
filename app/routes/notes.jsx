67b0111aa9f7c43ea6b9425e5898d86f8e20824a
Ashunar0 <asahi.gaia1530@gmail.com>
2025-06-01 17:45:16 +0900

    fixed home

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
 import "../styles/notes/notes.css";
+import { auth } from "../firebase";
+import { useNavigate } from "@remix-run/react";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Notes() {
+  const [user, setUser] = useState(auth.currentUser);
+
+  const navigate = useNavigate();
+
+  // ログインしていない場合はログイン画面にリダイレクト
+  // 難しいので解説は飛ばします
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        // ログアウトされてたらリダイレクト
+        navigate("/sign-in");
+      }
+    });
+
+    // クリーンアップ
+    return () => unsubscribe();
+  }, [navigate]);
+
+  // 読み込み中の表示
+  // ここも難しいので解説は飛ばします
+  if (!user) {
+    // 状態がまだ読み込まれてない場合（読み込み中）のフェイルセーフ
+    return <p>読み込み中...</p>;
+  }
+
   return (
     <div className="notes-page">
       <header>
